30c305da393577004673b73733d2b097ad2ba96e
Mark Wicks <mawicks@gmail.com>
2023-02-20 22:49:36 +0000

    Add working where clause test.

--- tests/dcorm/test_queries.py
+++ tests/dcorm/test_queries.py
@@ -35,7 +35,7 @@ def tables_created(empty_db):
 def relations_inserted(tables_created):
     # Insert two Foo records and
     # Two sets of two Bar records with each
-    # set Bar records pointing at a different
+    # set of Bar records pointing at a different
     # Foo record.
     for a in range(2):
         foo = Foo(a=a)
@@ -64,3 +64,18 @@ def test_empty_select_bar_returns_four_records(relations_inserted):
 
 def test_select_bar_join_foo_returns_four_records(relations_inserted):
     assert len(list(Select(Bar).join("some_foo")(relations_inserted))) == 4
+
+
+def test_select_bar_join_foo_where_specific_foo_returns_two_records(relations_inserted):
+    query = Select(Bar).join("some_foo").where("some_foo.a = 1")
+    results = list(query(relations_inserted))
+    assert len(results) == 2
+
+
+def test_select_bar_join_foo_where_specific_foo_returns_correct_records(
+    relations_inserted,
+):
+    query = Select(Bar).join("some_foo").where("some_foo.a = 1")
+    results = list(query(relations_inserted))
+    assert results[0].some_foo.a == 1
+    assert results[1].some_foo.a == 1
