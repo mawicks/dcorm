3ae7db3c97cd9dfdedc9267a3ffecb154dafcfb4
Mark Wicks <mawicks@gmail.com>
2023-02-26 03:06:31 +0000

    Add test_relation_queries.py after adding parametrize.

---
+++ tests/dcorm/test_relation_queries.py
@@ -0,0 +1,168 @@
+from dataclasses import dataclass
+import pytest
+import sqlite3
+
+from sandbox.dcorm.queries import Select
+from sandbox.dcorm.dcorm import orm_dataclass, create, insert
+
+
+@orm_dataclass
+@dataclass
+class Student:
+    name: str
+
+
+@orm_dataclass
+@dataclass
+class Instructor:
+    name: str
+
+
+@orm_dataclass
+@dataclass
+class Course:
+    title: str
+    instructor: Instructor
+
+
+@orm_dataclass
+@dataclass
+class Registration:
+    student: Student
+    course: Course
+
+
+class Registrations:
+    def __init__(self, connection):
+        self.connection = connection
+
+    def initialize_tables(self):
+        create(self.connection, Student, drop_if_exists=True)
+        create(self.connection, Instructor, drop_if_exists=True)
+        create(self.connection, Course, drop_if_exists=True)
+        create(self.connection, Registration, drop_if_exists=True)
+
+    def create_student(self, name) -> Student:
+        student = Student(name)
+        insert(self.connection, student)
+        return student
+
+    def create_instructor(self, name) -> Instructor:
+        instructor = Instructor(name)
+        insert(self.connection, instructor)
+        return instructor
+
+    def create_course(self, title, instructor) -> Course:
+        course = Course(title, instructor)
+        insert(self.connection, course)
+        return course
+
+    def register(self, student: Student, course: Course):
+        insert(self.connection, Registration(student, course))
+
+    def init(self):
+        self.initialize_tables()
+
+        alice = self.create_student("Alice")
+        bob = self.create_student("Bob")
+        charlie = self.create_student("Charlie")
+
+        jones = self.create_instructor("Jones")
+        kaplan = self.create_instructor("Kaplan")
+
+        algebra = self.create_course("Algebra", jones)
+        biology = self.create_course("Biology", kaplan)
+        calculus = self.create_course("Calculus", kaplan)
+
+        self.register(alice, algebra)
+        self.register(alice, biology)
+        self.register(alice, calculus)
+
+        self.register(bob, algebra)
+        self.register(bob, biology)
+
+        self.register(charlie, calculus)
+
+
+@pytest.fixture
+def empty_db():
+    return sqlite3.connect(":memory:")
+
+
+@pytest.fixture
+def registration_database(empty_db):
+    registrations = Registrations(empty_db)
+    registrations.init()
+    return empty_db
+
+
+# Tests for queries involving a relation
+
+
+@pytest.mark.parametrize(
+    "course,expected_students",
+    [
+        ("Algebra", {"Alice", "Bob"}),
+        ("Biology", {"Alice", "Bob"}),
+        ("Calculus", {"Alice", "Charlie"}),
+    ],
+)
+def test_class_list_query(registration_database, course, expected_students):
+    query = Select(Registration).join("course").where("course.title = ?", (course,))
+    class_list = [
+        registration.student.name for registration in query(registration_database)
+    ]
+    # Check the length before turning into a set to ensure there are no duplicates.
+    assert set(class_list) == expected_students
+
+
+@pytest.mark.parametrize(
+    "student,expected_courses",
+    [
+        ("Alice", {"Algebra", "Biology", "Calculus"}),
+        ("Bob", {"Algebra", "Biology"}),
+        ("Charlie", {"Calculus"}),
+    ],
+)
+def test_student_schedule_query(registration_database, student, expected_courses):
+    query = Select(Registration).join("student").where("student.name = ?", (student,))
+    class_list = [
+        registration.course.title for registration in query(registration_database)
+    ]
+    # Check the length before turning into a set to ensure there are no duplicates.
+    assert set(class_list) == expected_courses
+
+
+@pytest.mark.parametrize(
+    "instructor,expected_courses",
+    [
+        ("Kaplan", {"Biology", "Calculus"}),
+        ("Jones", {"Algebra"}),
+    ],
+)
+def test_instructor_schedule_query(registration_database, instructor, expected_courses):
+    query = (
+        Select(Course).join("instructor").where("instructor.name = ?", (instructor,))
+    )
+    class_list = [course.title for course in query(registration_database)]
+    # Check the length before turning into a set to ensure there are no duplicates.
+    assert set(class_list) == expected_courses
+
+
+@pytest.mark.parametrize(
+    "instructor,expected_students",
+    [
+        ("Jones", {"Alice", "Bob"}),
+        ("Kaplan", {"Alice", "Bob", "Charlie"}),
+    ],
+)
+def test_students_having_instructor(
+    registration_database, instructor, expected_students
+):
+    instructor_courses = (
+        Select(Course).join("instructor").where("instructor.name = ?", (instructor,))
+    )
+    instructor_registrations = Select(Registration).join("course", instructor_courses)
+    query = Select(Student).join(None, instructor_registrations, "student")
+    students = [student.name for student in query(registration_database)]
+    assert set(students) == expected_students
